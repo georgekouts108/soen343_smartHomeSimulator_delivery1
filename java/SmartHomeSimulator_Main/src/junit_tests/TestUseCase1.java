94a625d4e0e31cd5823faeae33e34c4bc76d4f2b
hackerrank <59776984+georgekouts108@users.noreply.github.com>
2020-10-20 14:12:34 -0400

    #9 attempted the first JUnit test case

--- java/SmartHomeSimulator_Main/src/junit_tests/TestUseCase1.java
+++ java/SmartHomeSimulator_Main/src/junit_tests/TestUseCase1.java
@@ -1,6 +1,12 @@
 package junit_tests;
 
 import house.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import utilities.*;
 import sample.*;
@@ -12,17 +18,98 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.testfx.api.FxToolkit;
-import javafx.*;
 
 import java.awt.*;
-//import org.testfx.framework.junit.ApplicationTest;
+import java.io.File;
+import java.io.FileNotFoundException;
+import org.testfx.framework.junit.*;
 
-public class TestUseCase1 {
+public class TestUseCase1 extends ApplicationTest {
+    private final Main main = new Main();
+    private final Controller controller = new Controller();
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setResizable(false);
+        Main.setMain_stage(primaryStage);
+
+        /**PROFILE SELECTION PAGE INITIALIZATION*/
+        Main.setProfileSelection(new AnchorPane());
+        Main.setProfileScene(new Scene(Main.getProfileSelection()
+                , Main.getLoginpageHeight(), Main.getLoginpageWidth()));
+
+        /**MODULE INITIALIZATIONS */
+        Main.setShsModule(new AnchorPane());
+        Main.setShcModule(new AnchorPane());
+        Main.setShpModule(new AnchorPane());
+        Main.setShhModule(new AnchorPane());
+
+        /**MAIN DASHBOARD INITIALIZATIONS */
+        Main.setMain_dashboard(new AnchorPane());
+        Main.createMainDashboardNecessities();
+
+        Main.setDashboardScene(new Scene(Main.getMain_dashboard(),
+                Main.getDashboardHeight(), Main.getDashboardWidth()));
+
+        /**EDIT SIMULATION CONTEXT INITIALIZATIONS */
+        Main.setEditContextLayout(new AnchorPane());
+        Main.setEditContextScene(new Scene(Main.getEditContextLayout(), 650, 650));
+        Main.setEditContextLayout2(new AnchorPane());
+        Main.setEditContextScene2(new Scene(Main.getEditContextLayout2(), 650, 650));
+
+        /**SET THE MAIN STAGE*/
+        Main.getMain_stage().setTitle("Smart Home Simulator - No user");
+        Main.getMain_stage().setScene(Main.getDashboardScene());
+        Main.getMain_stage().show();
+    }
+
 
     /**USE CASE #1 -- START AND STOP SIMULATOR*/
+    @Before
+    public void setup() throws Exception {
+        try {
+            Controller.createNewProfile(new TextField("P"), new RadioButton());
+
+            for (int a = 0; a < Main.getProfileSelection().getChildren().size(); a++) {
+                if (Main.getProfileSelection().getChildren().get(a).getId()
+                        .equals("hyperlinkForProfile"+Main.getProfiles()[0].getProfileID())) {
+                    Hyperlink loginLink = (Hyperlink) Main.getProfileSelection().getChildren().get(a);
+                    Controller.goToMainDashboardScene(Main.getProfiles()[0], loginLink);
+                    break;
+                }
+            }
+
+            Main.setHouseLayoutFile(new File("/Users/georgekoutsaris/Desktop/UNIVERSITY/Concordia/SEMESTERS/FALL 2020/SOEN 343/SOEN 343 PROJECT/soen343_project/housetest.txt"));
+            //House h = new House(Main.getHouseLayoutFileName());
+            //Main.setHouse(h);
+            //Main.setHouseLayout(h.getLayout());
+        }catch (Exception e){}
+
+    }
 
     @org.junit.Test
-    public void testCase1() { }
+    public void testUseCase1_1() {
+        for (int a = 0; a < Main.getMain_dashboard().getChildren().size(); a++) {
+            if (Main.getMain_dashboard().getChildren().get(a).getId().equals("simulationOnOffButton")) {
+                ToggleButton tb = (ToggleButton) Main.getMain_dashboard().getChildren().get(a);
+                tb.setSelected(true);
+                break;
+            }
+        }
+        assertEquals(true, Main.isSimulationIsOn());
+    }
+
+    @org.junit.Test
+    public void testUseCase1_2() {
+        for (int a = 0; a < Main.getMain_dashboard().getChildren().size(); a++) {
+            if (Main.getMain_dashboard().getChildren().get(a).getId().equals("simulationOnOffButton")) {
+                ToggleButton tb = (ToggleButton) Main.getMain_dashboard().getChildren().get(a);
+                tb.setSelected(false);
+                break;
+            }
+        }
+        assertEquals(false, Main.isSimulationIsOn());
+    }
 
 
 }
