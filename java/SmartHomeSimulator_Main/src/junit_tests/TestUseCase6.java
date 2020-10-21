e304389828f20e585f56789bf18c614c6f191d75
hackerrank <59776984+georgekouts108@users.noreply.github.com>
2020-10-20 23:37:20 -0400

    #9 test case 8

--- java/SmartHomeSimulator_Main/src/junit_tests/TestUseCase6.java
+++ java/SmartHomeSimulator_Main/src/junit_tests/TestUseCase6.java
@@ -2,6 +2,7 @@ package junit_tests;
 import javafx.application.Platform;
 //import com.sun.media.jfxmediaimpl.platform.Platform;
 import house.*;
+import javafx.collections.FXCollections;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
@@ -10,6 +11,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import utilities.*;
 import sample.*;
@@ -69,99 +71,10 @@ public class TestUseCase6 extends ApplicationTest {
         Main.getMain_stage().show();
     }
 
-//    @org.junit.Test
-//    public void testCase6() {
-//
-//        Platform.runLater(() -> {
-//            try {
-//                DatePicker datePicker = new DatePicker();
-//                TextField hourField = new TextField();
-//                TextField minuteField = new TextField();
-//                Label sim_date = new Label();
-//                Label sim_time = new Label();
-//
-//                for (int a = 0; a < Main.getMain_dashboard().getChildren().size(); a++) {
-//                    if (Main.getMain_dashboard().getChildren().get(a).getId().equals("datePicker")) {
-//                        datePicker = (DatePicker) Main.getMain_dashboard().getChildren().get(a);
-//                        datePicker.setValue(LocalDate.of(2020, 10, 25));
-//                        Main.getMain_dashboard().getChildren().set(a, datePicker);
-//                    }
-//                    else if (Main.getMain_dashboard().getChildren().get(a).getId().equals("hourField")) {
-//                        hourField = (TextField) Main.getMain_dashboard().getChildren().get(a);
-//                        hourField.setText("13");
-//                    }
-//                    else if (Main.getMain_dashboard().getChildren().get(a).getId().equals("minuteField")) {
-//                        minuteField = (TextField) Main.getMain_dashboard().getChildren().get(a);
-//                        minuteField.setText("45");
-//                    }
-//                    else if (Main.getMain_dashboard().getChildren().get(a).getId().equals("simulationDate")) {
-//                        sim_date = (Label) Main.getMain_dashboard().getChildren().get(a);
-//                    }
-//                    else if (Main.getMain_dashboard().getChildren().get(a).getId().equals("simulationTime")) {
-//                        sim_time = (Label) Main.getMain_dashboard().getChildren().get(a);
-//                    }
-//                }
-//
-//
-//                int second = 0;
-//                int minute = 0;
-//                int hour = 0;
-//                minute = Integer.parseInt(minuteField.getText());
-//                hour = Integer.parseInt(hourField.getText());
-//                for(int i = 0;i<=0;i++) {
-//                    Calendar cal = new GregorianCalendar();
-//                    DatePicker finalDatePicker = datePicker;
-//                    Label finalSim_date = sim_date;
-//                    Platform.runLater(()-> finalSim_date.setText(String.valueOf(finalDatePicker.getValue())));
-//                    if (i == 0) {
-//                        second = 0;
-//                        int finalSecond = second;
-//                        int finalHour = hour;
-//                        int finalMinute = minute;
-//                        Label finalSim_time = sim_time;
-//                        Platform.runLater(()-> finalSim_time.setText("Time "+(finalHour)%24+":"+(finalMinute)%60+":"+ finalSecond%60));
-//                        Thread.sleep(1000);
-//                    }
-//                    else
-//                    {
-//                        second = second + 1;
-//                        int finalSecond = second;
-//
-//                        if (finalSecond == 60){
-//                            minute = minute + 1;
-//                            second = 0;
-//                        }
-//                        int finalMinute = minute;
-//                        if (finalMinute == 60 && finalSecond == 60){
-//                            hour = hour + 1;
-//                            minute = 0;
-//                        }
-//                        int finalHour = hour;
-//                        if (finalHour == 23 && finalMinute == 60 && finalSecond == 60){
-//                            hour = 0;
-//                            minute = 0;
-//                            second = 0;
-//                        }
-//                        Label finalSim_time1 = sim_time;
-//                        Platform.runLater(()-> finalSim_time1.setText("Time "+(finalHour%24)+":"+(finalMinute%60)+":"+ finalSecond%60));
-//                        Thread.sleep(1000);
-//                    }
-//                }
-//
-//                boolean timeSuccessfullyUpdated =
-//                        ( sim_date.equals(datePicker.getValue()) &&
-//                                sim_time.equals("Time "+()%24+":"+(finalMinute)%60+":"+ finalSecond%60)
-//
-//                        );
-//
-//
-//
-//                assertEquals(true, timeSuccessfullyUpdated);
-//            }
-//            catch (Exception e){
-//                e.printStackTrace();
-//            }
-//        });
-//
-//    }
+    @org.junit.Test
+    public void testCase6() {
+
+    }
+
+
 }
--- java/SmartHomeSimulator_Main/src/junit_tests/TestUseCase8.java
+++ java/SmartHomeSimulator_Main/src/junit_tests/TestUseCase8.java
@@ -1,14 +1,247 @@
 package junit_tests;
-
+import javafx.application.Platform;
+//import com.sun.media.jfxmediaimpl.platform.Platform;
 import house.*;
+import javafx.collections.FXCollections;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import utilities.*;
 import sample.*;
 
 import org.junit.*;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-public class TestUseCase8 {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.testfx.framework.junit.*;
+
+public class TestUseCase8 extends ApplicationTest {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setResizable(false);
+        Main.setMain_stage(primaryStage);
+
+        /**PROFILE SELECTION PAGE INITIALIZATION*/
+        Main.setProfileSelection(new AnchorPane());
+        Main.setProfileScene(new Scene(Main.getProfileSelection(),
+                Main.getLoginpageHeight(), Main.getLoginpageWidth()));
+
+        /**MODULE INITIALIZATIONS */
+        Main.setShsModule(new AnchorPane());
+        Main.setShcModule(new AnchorPane());
+        Main.setShpModule(new AnchorPane());
+        Main.setShhModule(new AnchorPane());
+
+        /**MAIN DASHBOARD INITIALIZATIONS */
+        Main.setMain_dashboard(new AnchorPane());
+        Main.createMainDashboardNecessities();
+
+        Main.setDashboardScene(new Scene(Main.getMain_dashboard(),
+                Main.getDashboardHeight(), Main.getDashboardWidth()));
+
+        /**EDIT SIMULATION CONTEXT INITIALIZATIONS */
+        Main.setEditContextLayout(new AnchorPane());
+        Main.setEditContextScene(new Scene(Main.getEditContextLayout(), 650, 650));
+        Main.setEditContextLayout2(new AnchorPane());
+        Main.setEditContextScene2(new Scene(Main.getEditContextLayout2(), 650, 650));
+
+        /**SET THE MAIN STAGE*/
+        Main.getMain_stage().setTitle("Smart Home Simulator - No user");
+        Main.getMain_stage().setScene(Main.getDashboardScene());
+        Main.getMain_stage().show();
+    }
+
+    @Before
+    public void setup() {
+        try {
+            Controller.createNewProfile(new TextField("P"), new RadioButton()); // profile 1
+            Controller.createNewProfile(new TextField("C"), new RadioButton()); // profile 2
+
+            for (int a = 0; a < Main.getProfileSelection().getChildren().size(); a++) {
+                if (Main.getProfileSelection().getChildren().get(a).getId()
+                        .equals("hyperlinkForProfile" + Main.getProfiles()[0].getProfileID())) {
+                    Hyperlink loginLink = (Hyperlink) Main.getProfileSelection().getChildren().get(a);
+                    Controller.goToMainDashboardScene(Main.getProfiles()[0], loginLink);
+                    break;
+                }
+            }
+
+            House house = null;
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+
+            Main.setHouseLayoutFile(fileChooser.showOpenDialog(Main.getMain_stage()));
+            if (Main.getHouseLayoutFile() != null) {
+                Main.setHouseLayoutFileName(Main.getHouseLayoutFile().getName());
+                Main.setHouseLayoutFilePathName(Main.getHouseLayoutFile().getPath());
+                Main.getHouseLayoutFile().setReadOnly();
+                try {
+                    house = new House(Main.getHouseLayoutFilePathName());
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                }
+                Main.setHouseholdLocations(house.getRooms());
+                Main.setHouseLayout(house.getLayout());
+                Main.getHouseLayout().setPrefHeight(675);
+                Main.getHouseLayout().setPrefWidth(675);
+                Main.getHouseLayout().setId("houseLayout");
+                Main.getHouseLayout().setTranslateX(615);
+                Main.getHouseLayout().setTranslateY(10);
+                Main.getHouseLayout().setDisable(true);
+                Main.getMain_dashboard().getChildren().add(Main.getHouseLayout());
+                Main.createMainDashboardNecessities();
+            }
+
+            Label currentRoom = new Label("Your location: ");
+            currentRoom.setId("currentRoomOfLoggedInUser");
+            currentRoom.setTranslateY(200);
+            currentRoom.setTranslateX(450);
+            Main.getEditContextLayout().getChildren().add(currentRoom);
+
+            int transY = 120;
+            for (int r = 0; r < Main.getHouseholdLocations().length + 1; r++) {
+
+                if (r != Main.getHouseholdLocations().length) {
+                    Label l1 = new Label("# of people: " + Main.getHouseholdLocations()[r].getNumberOfPeopleInside());
+                    l1.setId("numOfPeopleInRoom" + Main.getHouseholdLocations()[r].getRoomID());
+                    l1.setTranslateX(120);
+                    l1.setTranslateY(transY + 5);
+                    int fr = r;
+                    Hyperlink hyp = new Hyperlink(Main.getHouseholdLocations()[r].getName());
+                    hyp.setId("hyperlinkForRoom-" + Main.getHouseholdLocations()[r].getName());
+                    hyp.setTranslateX(20);
+                    hyp.setTranslateY(transY);
+                    int finalR = r;
+                    hyp.setOnAction(e -> {
+
+                        // change the label of the number of people in the origin and destination rooms
+                        if ((Main.getCurrentActiveProfile() != null)) {
+                            if (Main.getCurrentLocation() == null) {
+                                Main.getHouseholdLocations()[fr].setNumberOfPeopleInside(Main.getHouseholdLocations()[fr].getNumberOfPeopleInside() + 1);
+
+                                // put that for loop here
+                                for (int i = 0; i < Main.getEditContextLayout().getChildren().size(); i++) {
+                                    try {
+                                        if (Main.getEditContextLayout().getChildren().get(i).getId().equals("numOfPeopleOutside")) {
+                                            Label label = (Label) Main.getEditContextLayout().getChildren().get(i);
+                                            int numberOfPeopleInHouse = 0;
+                                            int numberOfPeopleOutside;
+                                            for (int p = 0; p < Main.getHouseholdLocations().length; p++) {
+                                                numberOfPeopleInHouse += Main.getHouseholdLocations()[p].getNumberOfPeopleInside();
+                                            }
+                                            numberOfPeopleOutside = Main.getProfiles().length - numberOfPeopleInHouse;
+
+                                            label.setText("# of people: " + numberOfPeopleOutside);
+                                            Main.getEditContextLayout().getChildren().set(i, label);
+                                            break;
+                                        }
+                                    } catch (Exception ex) {
+                                    }
+                                }
+                            } else {
+                                if (!(Main.getCurrentLocation() == Main.getHouseholdLocations()[fr])) {
+                                    Main.getHouseholdLocations()[fr].setNumberOfPeopleInside(Main.getHouseholdLocations()[fr].getNumberOfPeopleInside() + 1);
+                                    Main.getCurrentLocation().setNumberOfPeopleInside(Main.getCurrentLocation().getNumberOfPeopleInside() - 1);
+                                }
+                            }
+                        }
+                        for (int j = 0; j < Main.getEditContextLayout().getChildren().size(); j++) {
+                            try {
+                                if (Main.getEditContextLayout().getChildren().get(j).getId().equals("numOfPeopleInRoom" + Main.getHouseholdLocations()[finalR].getRoomID())) {
+                                    Label label = (Label) Main.getEditContextLayout().getChildren().get(j);
+                                    label.setText("# of people: " + Main.getHouseholdLocations()[finalR].getNumberOfPeopleInside());
+                                    Main.getEditContextLayout().getChildren().set(j, label);
+                                    break;
+                                }
+                            } catch (Exception ex) {
+                            }
+                        }
+
+                        for (int i = 0; i < Main.getEditContextLayout().getChildren().size(); i++) {
+                            try {
+                                if (Main.getEditContextLayout().getChildren().get(i).getId().equals("numOfPeopleInRoom" + Main.getCurrentLocation().getRoomID())) {
+                                    Label label = (Label) Main.getEditContextLayout().getChildren().get(i);
+                                    label.setText("# of people: " + Main.getCurrentLocation().getNumberOfPeopleInside());
+                                    Main.getEditContextLayout().getChildren().set(i, label);
+                                    break;
+                                }
+                            } catch (Exception ex) {
+                            }
+                        }
+                        Main.setCurrentLocation(Main.getHouseholdLocations()[fr]);
+                        for (int a = 0; a < Main.getEditContextLayout2().getChildren().size(); a++) {
+                            try {
+                                if (Main.getEditContextLayout2().getChildren().get(a).getId().equals("currentRoomOfProfile" + Main.getCurrentActiveProfile().getProfileID())) {
+                                    Label label = (Label) Main.getEditContextLayout2().getChildren().get(a);
+                                    label.setText(Main.getCurrentLocation().getName());
+                                    Main.getEditContextLayout2().getChildren().set(a, label);
+                                    break;
+                                }
+                            } catch (Exception ex) {
+                            }
+                        }
+                        for (int i = 0; i < Main.getEditContextLayout().getChildren().size(); i++) {
+                            try {
+                                if (Main.getEditContextLayout().getChildren().get(i).getId().equals("currentRoomOfLoggedInUser")) {
+                                    Label label = (Label) Main.getEditContextLayout().getChildren().get(i);
+                                    label.setText("Your location:\n" + Main.getCurrentLocation().getName());
+                                    Main.getEditContextLayout().getChildren().set(i, label);
+                                    break;
+                                }
+                            } catch (Exception ex) {
+                            }
+                        }
+                        //========================================================================================================================
+                    });
+                    Main.getEditContextLayout().getChildren().addAll(hyp, l1);
+                    transY += 20;
+                }
+            }
+        }catch (Exception e){}
+    }
 
     @org.junit.Test
-    public void testCase8() { }
+    public void testCase8() {
+        Platform.runLater(()-> {
+
+            // for testing purposes
+            Room testRoom = new Room("Kitchen", 2, 1, 5, true);
+            Room testRoom2 = new Room("Bedroom", 1, 2, 2, true);
+            Room testRoom3 = new Room("Dining Room", 1, 1, 2, true);
+            Room testRoom4 = new Room("Bathroom", 2, 2, 5,  true);
+
+            Room[] test_rooms = new Room[]{testRoom, testRoom2, testRoom3, testRoom4};
+            Main.setHouseholdLocations(test_rooms);
+
+            // try moving the logged user to the Room at index 3
+            Main.setCurrentLocation(Main.getHouseholdLocations()[3]);
+            for (int a = 0; a < Main.getEditContextLayout().getChildren().size(); a++) {
+                if (Main.getEditContextLayout().getChildren().get(a).getId().equals("numOfPeopleInRoom"+Main.getHouseholdLocations()[3].getRoomID())) {
+                    Label label = (Label) Main.getEditContextLayout().getChildren().get(a);
+                    assertEquals(true, label.getText().equals("# of people: 1"));
+                    break;
+                }
+            }
+        });
+    }
 }
