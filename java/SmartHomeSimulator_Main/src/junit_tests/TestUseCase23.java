1c3d2766be51e8550e9f4746d19eca4953598be4
hackerrank <59776984+georgekouts108@users.noreply.github.com>
2020-12-02 04:21:19 -0500

    #62 unit test 23 implemented and passed

--- java/SmartHomeSimulator_Main/src/junit_tests/TestUseCase23.java
+++ java/SmartHomeSimulator_Main/src/junit_tests/TestUseCase23.java
@@ -1,6 +1,12 @@
 package junit_tests;
 
+import house.Room;
+import house.Zone;
+import org.junit.Assert;
 import org.testfx.framework.junit.*;
+import sample.Main;
+import sample.SHHModule;
+import sample.SHSHelpers;
 
 /**
  * Class for testing use case #23 - SHH displays current temperature of a room
@@ -10,6 +16,26 @@ public class TestUseCase23 extends ApplicationTest {
 
     @org.junit.Test
     public void testUC23() {
+        SHHModule shhModule = new SHHModule();
+        Room room1 = new Room("Room One", 1, 2, 1, true);
+        SHSHelpers.setHouseholdLocations(new Room[]{room1});
 
+        Zone testZone = new Zone();
+        testZone.setZoneRoomIDs(new int[]{room1.getRoomID()});
+        testZone.setZoneTemperature(10);
+        shhModule.setZones(new Zone[]{testZone});
+
+        SHSHelpers.setShhModuleObject(shhModule);
+
+        int temp = (int) SHSHelpers.getShhModuleObject().getZones()[0].getZoneTemperature();
+        Assert.assertEquals(10, temp);
+
+        testZone.setZoneTemperature(18);
+        shhModule.setZones(new Zone[]{testZone});
+
+        SHSHelpers.setShhModuleObject(shhModule);
+
+        int temp2 = (int) SHSHelpers.getShhModuleObject().getZones()[0].getZoneTemperature();
+        Assert.assertEquals(18, temp2);
     }
 }
