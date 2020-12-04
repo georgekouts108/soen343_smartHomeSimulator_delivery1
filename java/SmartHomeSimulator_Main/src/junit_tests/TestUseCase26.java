62bfe9f52b4323ab34677453a26ac00facba3234
Jason <jason.k99@hotmail.com>
2020-12-04 00:19:04 -0500

    Test case 26 and 27 complete

--- java/SmartHomeSimulator_Main/src/junit_tests/TestUseCase26.java
+++ java/SmartHomeSimulator_Main/src/junit_tests/TestUseCase26.java
@@ -1,6 +1,9 @@
 package junit_tests;
 
+import org.junit.Assert;
 import org.testfx.framework.junit.*;
+import sample.SHHModule;
+import sample.SHSHelpers;
 
 /**
  * Class for testing use case #26 - SHH detects when AWAY mode has been turned on
@@ -9,7 +12,22 @@ public class TestUseCase26 extends ApplicationTest {
 
     @org.junit.Test
     public void testUC26() {
+        SHHModule shhModule = new SHHModule();
+        SHSHelpers.setIs_away(true);
+        Assert.assertTrue(SHSHelpers.isIs_away());
 
+        shhModule.notifySHHOFAwayMode();
+        Assert.assertTrue(SHSHelpers.isIs_away());
+    }
+
+    @org.junit.Test
+    public void testUC26Failure() {
+        SHHModule shhModule = new SHHModule();
+        SHSHelpers.setIs_away(false);
+        Assert.assertFalse(SHSHelpers.isIs_away());
+
+        shhModule.notifySHHOFAwayMode();
+        Assert.assertFalse(SHSHelpers.isIs_away());
     }
 
 }
