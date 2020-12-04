3723543e7c59d337601ba7c4eef30f23c01ae444
hackerrank <59776984+georgekouts108@users.noreply.github.com>
2020-12-04 03:19:33 -0500

    #62 edited test cases 21 and 24

--- java/SmartHomeSimulator_Main/src/junit_tests/TestUseCase21.java
+++ java/SmartHomeSimulator_Main/src/junit_tests/TestUseCase21.java
@@ -16,22 +16,18 @@ public class TestUseCase21 extends ApplicationTest {
 
     @org.junit.Test
     public void testUC21() {
-        try {
-            SHHModule shhModule = new SHHModule();
-            Room room1 = new Room("Room One", 1, 2, 1, true);
-            Room room2 = new Room("Room Two", 1, 1, 1, false);
-            Room[] rooms = {room1, room2};
-            SHSHelpers.setHouseholdLocations(rooms);
-            Zone testZone1 = new Zone();
-            testZone1.setZoneRoomIDs(new int[]{room1.getRoomID()});
-            Zone testZone2 = new Zone();
-            testZone2.setZoneRoomIDs(new int[]{room2.getRoomID()});
-            shhModule.setZones(new Zone[]{testZone1, testZone2});
-            shhModule.setMaxNumOfZones(rooms.length);
-            SHSHelpers.setShhModuleObject(shhModule);
-            Assert.assertNotNull(shhModule);
-        }
-        catch (Exception e){}
-
+        SHHModule shhModule = new SHHModule();
+        Room room1 = new Room("Room One", 1, 2, 1, true);
+        Room room2 = new Room("Room Two", 1, 1, 1, false);
+        Room[] rooms = {room1, room2};
+        SHSHelpers.setHouseholdLocations(rooms);
+        Zone testZone1 = new Zone();
+        testZone1.setZoneRoomIDs(new int[]{room1.getRoomID()});
+        Zone testZone2 = new Zone();
+        testZone2.setZoneRoomIDs(new int[]{room2.getRoomID()});
+        shhModule.setZones(new Zone[]{testZone1, testZone2});
+        shhModule.setMaxNumOfZones(rooms.length);
+        SHSHelpers.setShhModuleObject(shhModule);
+        Assert.assertNotNull(shhModule);
     }
 }
--- java/SmartHomeSimulator_Main/src/junit_tests/TestUseCase24.java
+++ java/SmartHomeSimulator_Main/src/junit_tests/TestUseCase24.java
@@ -18,32 +18,21 @@ public class TestUseCase24 extends ApplicationTest {
 
     @org.junit.Test
     public void testUC24() {
-        try {
-            SHHModule shhModule = new SHHModule();
-
-            Room room1 = new Room("Room One", 1, 1, 1, true);
-            SHSHelpers.setHouseholdLocations(new Room[]{room1});
-
-            Zone testZone = new Zone();
-            testZone.setZoneRoomIDs(new int[]{room1.getRoomID()});
-            testZone.setZoneTemperature(15);
-            shhModule.setZones(new Zone[]{testZone});
-
-            SHSHelpers.setSimulationMonth(Month.JUNE);
-
-            TextField tf = new TextField();
-            tf.setText("5,7");
-            try {
-                shhModule.setWeatherMonthRange(tf,true);
-            }
-            catch (Exception e){}
-
-            SHSHelpers.setShhModuleObject(shhModule);
-            SHSHelpers.setOutsideTemperature(20);
-            shhModule.getZones()[0].setZoneTemperature(25);
-            shhModule.notifyToOpenAllZoneWindows();
-            Assert.assertNotNull(shhModule);
-        }
-        catch (Exception e){}
+        SHHModule shhModule = new SHHModule();
+        Room room1 = new Room("Room One", 1, 1, 1, true);
+        SHSHelpers.setHouseholdLocations(new Room[]{room1});
+        Zone testZone = new Zone();
+        testZone.setZoneRoomIDs(new int[]{room1.getRoomID()});
+        testZone.setZoneTemperature(15);
+        shhModule.setZones(new Zone[]{testZone});
+        SHSHelpers.setSimulationMonth(Month.JUNE);
+        TextField tf = new TextField();
+        tf.setText("5,7");
+        shhModule.setWeatherMonthRange(tf,true);
+        SHSHelpers.setShhModuleObject(shhModule);
+        SHSHelpers.setOutsideTemperature(20);
+        shhModule.getZones()[0].setZoneTemperature(25);
+        shhModule.notifyToOpenAllZoneWindows();
+        Assert.assertNotNull(shhModule);    
     }
 }
