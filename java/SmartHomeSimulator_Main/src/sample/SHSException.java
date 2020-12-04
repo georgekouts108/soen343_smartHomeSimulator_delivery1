72993230db6d2152f55ecd75f390f0f0ffcf76e9
hackerrank <59776984+georgekouts108@users.noreply.github.com>
2020-12-04 06:04:21 -0500

    #73 added javadoc comments in new exception class

--- java/SmartHomeSimulator_Main/src/sample/SHSException.java
+++ java/SmartHomeSimulator_Main/src/sample/SHSException.java
@@ -1,7 +1,14 @@
 package sample;
 
+/**
+ * Exception class for SHS-related exceptions
+ */
 public class SHSException extends Exception {
 
+    /**
+     * Constructor for SHS-Related exceptions
+     * @param errorMessage
+     */
     public SHSException(String errorMessage) {
         super(errorMessage);
     }
